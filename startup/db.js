2ef63262c598269a083aeeda1a56b2a2e2363df7
faraza753 <faraza753@email.com~>
2024-02-01 21:59:02 +0530

    new

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 module.exports = () => {
-    mongoose.connect('mongodb+srv://faraz:BlackPanther12@cluster.0q32rvc.mongodb.net/?retryWrites=true&w=majority', {
+    mongoose.connect('mongodb+srv://enter the connection string', {
         
     }).then(() => console.log('Mongo Db Connected.....'));
 }
